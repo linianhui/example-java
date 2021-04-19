3ad021e736f3b70bec8e1442bf8cba462a6f000b
lnh <lnhdyx@outlook.com>
2021-04-19 14:04:19 +0800

    add echo

--- io/src/main/java/io/BIOEcho.java
+++ io/src/main/java/io/BIOEcho.java
@@ -1,8 +1,6 @@
 package io;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
@@ -10,20 +8,20 @@ import java.net.SocketAddress;
 public class BIOEcho implements Echo {
 
     public void echo(ServerSocket serverSocket) throws IOException {
-        int readSize = 0;
-        byte[] buf = new byte[256];
         while (true) {
             Socket socket = serverSocket.accept();
             SocketAddress clientAddress = socket.getRemoteSocketAddress();
             System.out.println("\naccept client " + clientAddress);
-            InputStream in = socket.getInputStream();
-            OutputStream out = socket.getOutputStream();
-            while ((readSize = in.read(buf))!=-1) {
-                System.out.print("\nread form client : ");
-                System.out.print(new String(buf, 0, readSize));
-                out.write(buf, 0, readSize);
-            }
+            echo(socket);
             socket.close();
         }
     }
+
+    private void echo(Socket socket) throws IOException {
+        int readSize = 0;
+        do {
+            readSize = echoCore(socket);
+        } while (readSize!=-1);
+    }
+
 }
--- io/src/main/java/io/Echo.java
+++ io/src/main/java/io/Echo.java
@@ -1,8 +1,31 @@
 package io;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public interface Echo {
     void echo(ServerSocket serverSocket) throws IOException;
+
+    default int echoCore(
+            Socket socket
+    ) throws IOException {
+        final InputStream in = socket.getInputStream();
+        final byte[] buf = new byte[256];
+        int readSize = in.read(buf);
+        if (readSize==-1) {
+            return readSize;
+        }
+        String str = new String(buf, 0, readSize - 1, StandardCharsets.UTF_8);
+        System.out.printf("read form client : %s", str);
+        String upperCase = str.toUpperCase();
+
+        final OutputStream out = socket.getOutputStream();
+        out.write(upperCase.getBytes(StandardCharsets.UTF_8));
+        System.out.printf("write to client : %s", upperCase);
+        return readSize;
+    }
 }
